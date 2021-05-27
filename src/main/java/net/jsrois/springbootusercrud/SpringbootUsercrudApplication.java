ab95192f27ea261157a4415dbaf0682d2607ef0a
Javi <javi@jsrois.net>
2021-05-27 08:07:14 +0200

    Created basic page and added some postgresql support

---
+++ src/main/java/net/jsrois/springbootusercrud/SpringbootUsercrudApplication.java
@@ -0,0 +1,13 @@
+package net.jsrois.springbootusercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUsercrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUsercrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/jsrois/springbootusercrud/SpringbootUsercrudApplicationTests.java
@@ -0,0 +1,14 @@
+package net.jsrois.springbootusercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUsercrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
