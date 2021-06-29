0336283f4fb1410b6af08452f6853bbf97947db4
Javi <javi@jsrois.net>
2021-06-29 12:18:37 +0200

    Fixed data script

--- src/main/resources/no_data.sql
+++ src/main/resources/no_data.sql
@@ -1,16 +1,19 @@
+INSERT INTO categories(id, name)
+VALUES (1, 'Fantasy'),
+       (2, 'Politics'),
+       (3, 'Software'),
+       (4, 'Essay'),
+       (5, 'Philosophy');
+
 INSERT INTO books(title, author, category_id)
 VALUES ('Una habitación propia', 'Virginia Woolf', 4),
        ('Un mago de Terramar', 'Ursula K. Leguin', 1),
        ('Los desposeídos', 'Ursula K. Leguin', 1),
        ('Lean Software Development', 'Mary Poppendieck', 3),
        ('Women, Race and Class', 'Angela Y. Davis', 4),
-       ('Object Design', 'Rebecca Wirfs-Brock', 3);
-
--- INSERT INTO categories(name)
---     VALUES ('Fantasy'),
---            ('Politics'),
---            ('Software'),
---            ('Essay');
+       ('Object Design', 'Rebecca Wirfs-Brock', 3),
+       ('Utopía no es una isla', 'Layla Martínez', 4),
+       ('Utopía', 'Tomás Moro', 5);
 
 
 
